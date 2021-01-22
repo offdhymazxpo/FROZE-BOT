d225c8de107101fa3f4551f79dd612c650b7e640
offdhymazxpo <76859255+offdhymazxpo@users.noreply.github.com>
2021-01-22 20:58:17 +0800

    donasi.js

--- src/donasi.js
+++ src/donasi.js
@@ -2,8 +2,8 @@ const donasi = (prefix) => {
 	return `
 	*Alhamdulillah Kak Mau Donasi Ke Saya ☺️*
 	*[VIA]*
-	*->[PULSA] 081539336834 [INDOSAT]*
-	*->[GOPAY] SOON*
+	*->[PULSA] 085796582392 [INDOSAT]*
+	*->[DANA] 085796582392*
 	*[MADE BY 🥶FROZE BOT🥶]*`
  }
  
